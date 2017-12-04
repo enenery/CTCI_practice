3f95f1feec926daec5206b5e0b90245945fd1835
enenery <erii.sugimoto@stu.bmcc.cuny.edu>
2017-12-03 21:17:10 -0500

    test CheckBalancedTree

--- tree/java/CheckBalancedTree.java
+++ tree/java/CheckBalancedTree.java
@@ -1,13 +1,34 @@
 /**
 4.1 Check Balanced Tree from CTCI 4
-Implement a function to check if a tree is balanced. For the purposes of this question,
-a balanced tree is de!ned to be a tree such that no two leaf nodes di#er in distance
+Challenge: Implement a function to check if a tree is balanced. For the purposes of this question,
+a balanced tree is defined to be a tree such that no two leaf nodes difer in distance
 from the root by more than one.
-@comment to be modified and tested
+
 @since 11/27/2017
 */
-
+class TreeNode{
+	TreeNode left;
+	TreeNode right;
+	int val;
+	public TreeNode(int x){
+		val = x;
+		left = right = null;
+	}
+}
 public class CheckBalancedTree{
+	public static void main(String args[]){
+		TreeNode t1 = new TreeNode(0);
+		TreeNode c1 = t1;
+		c1.left = new TreeNode(1);
+		c1.left.left = new TreeNode(2);
+		c1.left.right = new TreeNode(2);
+		//c1.right = new TreeNode(1);
+		//c1.right.left = new TreeNode(2);
+		//c1.right.right = new TreeNode(2);
+		
+		System.out.println(checkBalancedTree(t1));
+	}
+	
 	public static int maxDepth(TreeNode root){
 		if(root == null)
 			return 0;
@@ -25,7 +46,5 @@ public class CheckBalancedTree{
 	public static boolean checkBalancedTree(TreeNode root){
 		return (maxDepth(root) - minDepth(root) <= 1);
 	}
-	public static void main(String args[]){
-		
-	}
+	
 }
