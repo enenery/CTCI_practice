fece4f4b920f4dfb8023c3d7858f7e015ba46749
erii.sugimoto@stu.bmcc.cuny.edu <erii.sugimoto@stu.bmcc.cuny.edu>
2018-03-06 17:18:17 -0500

    Add java dir in recursion dir

---
+++ recursion/java/MaximumSubarray.java
@@ -0,0 +1,42 @@
+import java.util.*;
+//with the help from Geek For Geeks
+public class MaximumSubarray{
+	public static int maxSubArraySum(int arr[], int l, int h){
+		if(l == h)
+			return arr[l];
+		int m = (l + h)/2;
+		return Math.max(Math.max(maxSubArraySum(arr, l, m), maxSubArraySum(arr, m+1, h)), maxCrossingSum(arr, l, m, h));
+	}
+	
+	static int maxCrossingSum(int arr[], int l, int m, int h)
+    {
+        int sum = 0;
+        int leftSum = Integer.MIN_VALUE;
+        for (int i = m; i >= l; i--)
+        {
+            sum = sum + arr[i];
+            if (sum > leftSum)
+            leftSum = sum;
+        }
+ 
+        sum = 0;
+        int rightSum = Integer.MIN_VALUE;
+        for (int i = m + 1; i <= h; i++)
+        {
+            sum = sum + arr[i];
+            if (sum > rightSum)
+            rightSum = sum;
+        }
+ 
+        return leftSum + rightSum;
+    }
+	
+	public static void main(String args[]){
+		//int arr[] = {0, -2, 1, -2, 1, 1, 1, 2, -2, 0, -1};
+		//int[] arr = {0, 13, -3, -25, 20, -3, -16, -23};
+		int[] arr = {0, 13};
+		int lastIndex = arr.length;
+		int maxSum = maxSubArraySum(arr, 0, lastIndex-1);
+		System.out.println("Max contiguous sum = " + maxSum);
+	}
+}
