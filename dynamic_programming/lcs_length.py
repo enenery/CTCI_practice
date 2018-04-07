e2650d1a0646446b39d744b6714585b0f8798932
Erii Sugimoto <eriisugimoto1293@gmail.com>
2018-04-07 13:35:58 -0400

    Fix errors and add printing

--- dynamic_programming/lcs_length.py
+++ dynamic_programming/lcs_length.py
@@ -1,21 +1,51 @@
 def lcs_length(x, y):
 	m = len(x)
 	n = len(y)
-	b = [0 for i in range(1, n + 1)]
-	c = [0 for i in range(n + 1)]
+	print("m = " + str(m))
+	print("n = " + str(n))
+	b = [[0 for i in range(m + 1)] for j in range(n + 1)]
+	c = [[0 for i in range(m + 1)] for j in range(n + 1)]
+	print("b = " + str(len(b)))
+	print("c = " + str(len(c)))
 	
 	for i in range(1, m):
-		for j in range(1,n):
+		for j in range(1, n):
+			print("i = " + str(i))
+			print("j = " + str(j))
 			if x[i] == y[j]:
-				c[i, j] = c[i - 1, j - 1] + 1
-				b[i, j] = "↖"
-			elif c[i - 1, j] >= c[i, j - 1]:
-				b[i, j] = "↑"
+				c[i][j] = c[i - 1][j - 1] + 1
+				b[i][j] = "↖"
+			elif c[i - 1][j] >= c[i][j - 1]:
+				c[i][j] = c[i - 1][j]
+				b[i][j] = "↑"
 			else: 
-				c[i, j] = c[i, j - 1]
-				b[i, j] = "←"
+				c[i][j] = c[i][j - 1]
+				b[i][j] = "←"
 	return c, b
 
+def print_lcs(b, x, i, j):
+	print("i = " + str(i))
+	print("j = " + str(j))
+	if i == 0 or j == 0:
+		return
+	if b[i][j] == "↖":
+		print_lcs(b, x, i-1, j-1)
+		print(x[i])
+	elif b[i][j] == "↑":
+		print_lcs(b, x, i-1, j)
+	else:
+		print_lcs(b, x, i, j-1)
 
-arrow = '\u2196'
-print(arrow)
+def print_matrix(x):
+	for i in range(len(x)):
+		for j in range(len(x)):
+			print(x[i][j], end="")
+		print("")
+
+x = [' ', 'a', 'b', 'c', 'b', 'd', 'a', 'b']
+y = [' ', 'b', 'd', 'c', 'a', 'b', 'a']
+
+c, b = lcs_length(x, y)
+print_matrix(b)
+print_matrix(c)
+print_lcs(b, x, len(x) - 1, len(y) - 1)
